8f699bfaeb2ef65ae51eb8147a73714c8468f7f8
Michael Putnam <mikeputnam713@gmail.com>
2022-02-05 19:13:16 -0800

    Complete Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,33 @@
 class Unicorn {
-  constructor(name, color) {
+
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+
+
+  isWhite() {
+    return this.isWhite = false
+  }
+
+
+  says(sparklyStuff) {
+    var output = `**;* ${sparklyStuff} *;**`
+    return output
+  }
+  // isWhite(color) {
+  //
+  //  this.color =  ;
+  //
+  //
+  // }
+
+
+
+
+
 }
+
+
 
 module.exports = Unicorn;
